5ec95c50843e44196e678398be25f9ac84b6eac8
Matias Fontanini <matias.fontanini@gmail.com>
2023-10-09 11:31:20 -0700

    Restructure user input handling code

--- src/input/user.rs
+++ src/input/user.rs
@@ -1,5 +1,5 @@
 use crossterm::event::{poll, read, Event, KeyCode, KeyEvent, KeyModifiers};
-use std::{io, time::Duration};
+use std::{io, mem, time::Duration};
 
 #[derive(Default)]
 pub struct UserInput {
@@ -8,94 +8,82 @@ pub struct UserInput {
 
 impl UserInput {
     pub fn poll_next_command(&mut self, timeout: Duration) -> io::Result<Option<UserCommand>> {
-        if poll(timeout)? { self.next_command() } else { Ok(None) }
+        if poll(timeout)? {
+            self.next_command()
+        } else {
+            Ok(None)
+        }
     }
 
     pub fn next_command(&mut self) -> io::Result<Option<UserCommand>> {
-        let command = match read()? {
-            Event::Key(event) => self.handle_key_event(&event),
-            Event::Resize(..) => Some(UserCommand::Redraw),
-            _ => None,
+        let current_state = mem::take(&mut self.state);
+        let (command, next_state) = match read()? {
+            Event::Key(event) => Self::apply_key_event(event, current_state),
+            Event::Resize(..) => (Some(UserCommand::Redraw), current_state),
+            _ => (None, current_state),
         };
-        if command.is_some() {
-            self.state.reset();
-        }
+        self.state = next_state;
         Ok(command)
     }
 
-    fn handle_key_event(&mut self, event: &KeyEvent) -> Option<UserCommand> {
+    fn apply_key_event(event: KeyEvent, state: InputState) -> (Option<UserCommand>, InputState) {
         match event.code {
             KeyCode::Char('h') | KeyCode::Char('k') | KeyCode::Left | KeyCode::PageUp | KeyCode::Up => {
-                Some(UserCommand::JumpPreviousSlide)
+                (Some(UserCommand::JumpPreviousSlide), InputState::Empty)
             }
             KeyCode::Char('l')
             | KeyCode::Char('j')
             | KeyCode::Right
             | KeyCode::PageDown
             | KeyCode::Down
-            | KeyCode::Char(' ') => Some(UserCommand::JumpNextSlide),
-            KeyCode::Char('c') if event.modifiers == KeyModifiers::CONTROL => Some(UserCommand::Exit),
-            KeyCode::Char('G') => self.handle_uppercase_g(),
-            KeyCode::Char('g') => self.handle_lowercase_g(),
+            | KeyCode::Char(' ') => (Some(UserCommand::JumpNextSlide), InputState::Empty),
+            KeyCode::Char('c') if event.modifiers == KeyModifiers::CONTROL => {
+                (Some(UserCommand::Exit), InputState::Empty)
+            }
+            KeyCode::Char('G') => Self::apply_uppercase_g(state),
+            KeyCode::Char('g') => Self::apply_lowercase_g(state),
             KeyCode::Char(number) if number.is_ascii_digit() => {
                 let number = number.to_digit(10).expect("not a digit");
-                self.handle_number(number);
-                None
+                (None, Self::apply_number(number, state))
             }
-            _ => {
-                self.state.reset();
-                None
-            }
+            _ => (None, InputState::Empty),
         }
     }
 
-    fn handle_lowercase_g(&mut self) -> Option<UserCommand> {
-        match self.state {
-            InputState::PendingG => Some(UserCommand::JumpFirstSlide),
-            InputState::Empty => {
-                self.state = InputState::PendingG;
-                None
-            }
-            _ => {
-                self.state.reset();
-                None
-            }
+    fn apply_lowercase_g(state: InputState) -> (Option<UserCommand>, InputState) {
+        match state {
+            InputState::PendingG => (Some(UserCommand::JumpFirstSlide), InputState::Empty),
+            InputState::Empty => (None, InputState::PendingG),
+            _ => (None, InputState::Empty),
         }
     }
 
-    fn handle_uppercase_g(&mut self) -> Option<UserCommand> {
-        match self.state {
-            InputState::Empty => Some(UserCommand::JumpLastSlide),
-            InputState::PendingNumber(number) => Some(UserCommand::JumpSlide(number)),
-            _ => {
-                self.state.reset();
-                None
-            }
+    fn apply_uppercase_g(state: InputState) -> (Option<UserCommand>, InputState) {
+        match state {
+            InputState::Empty => (Some(UserCommand::JumpLastSlide), InputState::Empty),
+            InputState::PendingNumber(number) => (Some(UserCommand::JumpSlide(number)), InputState::Empty),
+            _ => (None, InputState::Empty),
         }
     }
 
-    fn handle_number(&mut self, number: u32) {
-        let maybe_next = match self.state {
+    fn apply_number(number: u32, state: InputState) -> InputState {
+        let maybe_next = match state {
             InputState::PendingNumber(current) => current.checked_mul(10).and_then(|n| n.checked_add(number)),
             InputState::Empty => Some(number),
-            InputState::OverflowedNumber => {
-                return;
-            }
             _ => {
-                self.state.reset();
-                return;
+                return InputState::Empty;
             }
         };
         // If we overflowed, jump to a terminal state that indicates so. This way 123123123G is not
         // an alias for G
         match maybe_next {
-            Some(number) => self.state = InputState::PendingNumber(number),
-            None => self.state = InputState::OverflowedNumber,
-        };
+            Some(number) => InputState::PendingNumber(number),
+            None => InputState::OverflowedNumber,
+        }
     }
 }
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq, Eq)]
 pub enum UserCommand {
     Redraw,
     JumpNextSlide,
@@ -106,7 +94,7 @@ pub enum UserCommand {
     Exit,
 }
 
-#[derive(Default)]
+#[derive(Default, Debug, PartialEq, Eq)]
 enum InputState {
     #[default]
     Empty,
@@ -115,8 +103,42 @@ enum InputState {
     OverflowedNumber,
 }
 
-impl InputState {
-    fn reset(&mut self) {
-        *self = InputState::Empty;
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn lowercase_g() {
+        let state = InputState::Empty;
+        let (command, state) = UserInput::apply_key_event(KeyCode::Char('g').into(), state);
+        assert!(command.is_none());
+
+        let (command, state) = UserInput::apply_key_event(KeyCode::Char('g').into(), state);
+        assert_eq!(command, Some(UserCommand::JumpFirstSlide));
+        assert_eq!(state, InputState::Empty);
+    }
+
+    #[test]
+    fn uppercase_g() {
+        let state = InputState::Empty;
+        let (command, state) = UserInput::apply_key_event(KeyCode::Char('G').into(), state);
+        assert_eq!(command, Some(UserCommand::JumpLastSlide));
+        assert_eq!(state, InputState::Empty);
+    }
+
+    #[test]
+    fn jump_number() {
+        let state = InputState::Empty;
+        let (command, state) = UserInput::apply_key_event(KeyCode::Char('1').into(), state);
+        assert!(command.is_none());
+        assert_eq!(state, InputState::PendingNumber(1));
+
+        let (command, state) = UserInput::apply_key_event(KeyCode::Char('2').into(), state);
+        assert!(command.is_none());
+        assert_eq!(state, InputState::PendingNumber(12));
+
+        let (command, state) = UserInput::apply_key_event(KeyCode::Char('G').into(), state);
+        assert_eq!(command, Some(UserCommand::JumpSlide(12)));
+        assert_eq!(state, InputState::Empty);
     }
 }
