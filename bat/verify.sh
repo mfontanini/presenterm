d3292436378f4f8b374578aaa7a999b80ac384e3
Matias Fontanini <matias.fontanini@gmail.com>
2023-12-08 16:13:57 -0800

    Edd .editorconfig for *.sh and format shell scripts

--- bat/verify.sh
+++ bat/verify.sh
@@ -12,15 +12,13 @@ git clone https://github.com/sharkdp/bat.git "$clone_path"
 cd "$clone_path"
 git reset --hard "$git_hash"
 
-for file in syntaxes.bin themes.bin
-do
-  our_hash=$(sha256sum "$script_dir/$file" | cut -d " " -f1)
-  their_hash=$(sha256sum "$clone_path/assets/$file" | cut -d " " -f 1)
-  if [ "$our_hash" != "$their_hash" ]
-  then
-    echo "Unexpected hash for ${file}: should be ${their_hash}, is ${our_hash}"
-    exit 1
-  fi
+for file in syntaxes.bin themes.bin; do
+    our_hash=$(sha256sum "$script_dir/$file" | cut -d " " -f1)
+    their_hash=$(sha256sum "$clone_path/assets/$file" | cut -d " " -f 1)
+    if [ "$our_hash" != "$their_hash" ]; then
+        echo "Unexpected hash for ${file}: should be ${their_hash}, is ${our_hash}"
+        exit 1
+    fi
 done
 
 echo "All hashes match"
