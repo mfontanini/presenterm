f74616095232fc3526f6e745207c49c3a77bcdb2
Matias Fontanini <matias.fontanini@gmail.com>
2023-12-17 11:27:16 -0800

    Use `viuer`'s computed image dimensions to sync cursor

--- src/render/media.rs
+++ src/render/media.rs
@@ -67,7 +67,7 @@ impl MediaRender {
         image: &Image,
         position: CursorPosition,
         dimensions: &WindowSize,
-    ) -> Result<(), RenderImageError> {
+    ) -> Result<(u32, u32), RenderImageError> {
         if !dimensions.has_pixels {
             return Err(RenderImageError::NoWindowSize);
         }
@@ -108,11 +108,11 @@ impl MediaRender {
         //
         // This switch is because otherwise `viuer::print_from_file` for kitty/ascii blocks will
         // re-read the image every time.
-        match (&self.mode, source) {
+        let dimensions = match (&self.mode, source) {
             (TerminalMode::Iterm2, ImageSource::Filesystem(image_path)) => viuer::print_from_file(image_path, &config)?,
             (TerminalMode::Other, _) | (_, ImageSource::Generated) => viuer::print(image, &config)?,
         };
-        Ok(())
+        Ok(dimensions)
     }
 }
 
