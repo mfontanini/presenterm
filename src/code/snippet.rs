578519f277a87b08371aa47477134f8b61d86559
JanNeuendorf <jan.neuendorf@uni-muenster.de>
2025-07-02 10:33:56 +0200

    uv as an alternative executor

--- src/code/snippet.rs
+++ src/code/snippet.rs
@@ -630,7 +630,6 @@ impl FromStr for SnippetLanguage {
             "toml" => Toml,
             "typescript" | "ts" => TypeScript,
             "typst" => Typst,
-            "uv" => Python,
             "xml" => Xml,
             "yaml" => Yaml,
             "verilog" => Verilog,
